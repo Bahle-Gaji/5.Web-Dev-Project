4b016bb89c29e4e1c519ca5534d6fab04161b03b
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-06-29 06:09:34 +0200

    Create and Link Review CRUD Operations

--- movie-reviews/backend/index.js
+++ movie-reviews/backend/index.js
@@ -4,6 +4,7 @@ import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from 'dotenv';
 import MoviesDAO from "./dao/moviesDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
 async function main() {
 
@@ -22,6 +23,8 @@ async function main() {
 
         //gets initial reference to collection and db
         await MoviesDAO.injectDB(client);
+
+        await ReviewsDAO.injectDB(client);
 
         app.listen(port, () => {
             console.log('server is running on port:' + port);
