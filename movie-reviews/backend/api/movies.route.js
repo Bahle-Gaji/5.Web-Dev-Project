36c5f78148d2089891744541dcc25a9de9120da1
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-06-29 04:55:08 +0200

    Create Movies getAPI Controller

--- movie-reviews/backend/api/movies.route.js
+++ movie-reviews/backend/api/movies.route.js
@@ -1,8 +1,9 @@
 "use strict";
 
 import express from 'express';
+import MoviesController from './movies.controller';
 
 const router = express.Router() // get access to express router
-router.route('/').get((req,res) => res.send('hello world'))
+router.route('/').get(MoviesController.apiGetMovies)
 
 export default router
