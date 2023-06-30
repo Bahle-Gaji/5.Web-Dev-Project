8e391af25b648f6afb2d5d9136a4f9e54c8de0ba
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-06-30 04:53:38 +0200

    Create Navbar, Link Movie route and Login route

--- movie-reviews/frontend/src/App.js
+++ movie-reviews/frontend/src/App.js
@@ -6,11 +6,25 @@ import Movie from './components/movie';
 import Movieslist from './components/movies-list';
 import Login from './components/login';
 import AddReview from './components/add-review';
+import { Nav, Navbar } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link><Link to={"/movies"}>Movies</Link></Nav.Link>
+            <Nav.Link>
+              {
+                false ? (<a>Logout User</a>) : (<Link to={'/login'}>Login</Link>)
+              }
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
