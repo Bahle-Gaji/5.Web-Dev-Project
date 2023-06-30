3e749f61ddfdd07eacb7d1629acc6db514d59156
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-06-30 04:39:50 +0200

    Create frontend Dir, Install Dependancies

---
+++ movie-reviews/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
