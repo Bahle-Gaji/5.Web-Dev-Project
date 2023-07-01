45e00157984add06385ba63c3a78b69bfd1ce04b
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-07-01 05:30:48 +0200

    Create Login Component Form

--- movie-reviews/frontend/src/components/login.js
+++ movie-reviews/frontend/src/components/login.js
@@ -1,9 +1,55 @@
-import React from "react";
+import { React, useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
+const Login = (props) => {
+    
+    const [name, setName] = useState('');
+    const [id, setId] = useState('');
+
+    const onChangeName = e => {
+        const name = e.target.value
+        setName(name);
+    }
+
+    const onChangeId = e => {
+        const id = e.target.value
+        setId(id);
+    }
+
+    const nav = useNavigate();
+    const login = () => {
+        props.login({ name: name, id: id })
+        nav('/');
+    }
+
     return (
         <div className="App">
-            Login
+            <Form>
+                <Form.Group>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter username"
+                        value={name}
+                        onChange={onChangeName}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>ID</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter id"
+                        value={id}
+                        onChange={onChangeId}
+                    />
+                </Form.Group>
+                <Button variant="primary" onClick={login}>
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
+
+export default Login;
