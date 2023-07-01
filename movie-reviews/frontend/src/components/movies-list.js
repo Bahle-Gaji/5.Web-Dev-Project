f1dea70ab6541e71305d504afe0645f48d2ff90e
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-07-01 03:09:39 +0200

    Create hook to Retrieve Movies and Ratings

--- movie-reviews/frontend/src/components/movies-list.js
+++ movie-reviews/frontend/src/components/movies-list.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MoviesDataService from '../services/movies';
+import { Link } from 'react-router-dom';
 
-export default function Movieslist() {
+const MoviesList = (props) => {
+    const [movies, setMovies] = useState([])
+    const [searchTitle, setSearchTitle] = useState("")
+    const [searchRating, setSearchRating] = useState("")
+    const [ratings, setRatings] = useState(["All Ratings"])
+
+    useEffect(() => {
+        retrieveMovies()
+        retrieveRatings()
+    }, [])
+
+    const retrieveMovies = () => {
+        MoviesDataService.getAll()
+            .then(res => {
+                console.log(res.data)
+                setMovies(res.data.movies)
+            })
+            .catch(e =>
+                console.log(e)
+            )
+    }
+
+    const retrieveRatings = () => {
+        MoviesDataService.getRatings()
+            .then(res => {
+                console.log(res.data)
+                //start with 'All ratings' if user doesn't specify any ratings
+                setRatings(['All Ratings'].concat(res.data))
+            })
+            .catch(e => console.log(e))
+    }
+
     return (
         <div className="App">
             Movies List
         </div>
     )
 }
+
+export default MoviesList;
