8094cc453cad86834505c991e59c405400e3b3de
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-05 22:59:44 +0200

    Initialize React App

---
+++ e-commerce/src/App.js
@@ -0,0 +1,8 @@
+import Home from './pages/Home.js';
+
+const App = () => {
+    return <Home/>
+}
+
+export default App;
+
---
+++ e-commerce/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Navbar = () => {
+  return (
+    <Container>
+     
+    </Container>
+  )
+}
+
+export default Navbar;
