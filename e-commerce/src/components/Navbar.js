de5fae7d12a1ae7fec671b88889af1095fac191f
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-05 23:00:42 +0200

    Create navbar items

--- e-commerce/src/components/Navbar.js
+++ e-commerce/src/components/Navbar.js
@@ -1,11 +1,80 @@
 import React from 'react';
+import styled from 'styled-components';
+import {Badge, MailIcon, Search } from '@mui/icons-material';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgrey;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input`
+  border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`;
 
 const Navbar = () => {
   return (
     <Container>
-     
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input></Input>
+            <Search></Search>
+          </SearchContainer>
+        </Left>
+
+        <Center>
+          <Logo>Bahle.</Logo>
+        </Center>
+
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon color="action" />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
     </Container>
   )
 }
