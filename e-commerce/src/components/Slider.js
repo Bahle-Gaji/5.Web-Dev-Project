66a2091fe1dbaedc21f93074fdc33a2adcb3e73a
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-09 23:16:51 +0200

    Initialize Slider Component

---
+++ e-commerce/src/components/Slider.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background-color: aliceblue;
+`;
+
+const Slider = () => {
+    return (
+        <div>
+            <Container>
+
+            </Container>
+        </div>
+    )
+}
+
+export default Slider;
