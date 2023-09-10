de2bbf22661c076e05a32d24ef1ca537f2f8342b
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-09-10 04:49:47 +0200

    Create Arrow icons using Styled Components.

--- e-commerce/src/components/Slider.js
+++ e-commerce/src/components/Slider.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+    ArrowLeftOutlined as ArrowLeftOutlinedIcon,
+    ArrowRightOutlined as ArrowRightOutlinedIcon
+} from '@mui/icons-material';
 
 const Container = styled.div`
-    background-color: aliceblue;
+    width: 100%;
+    height: 1100vh;
+    display: flex;
+
 `;
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #fff7f7;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 const Slider = () => {
     return (
         <div>
             <Container>
+                <Arrow>
+                    <ArrowLeftOutlinedIcon />
+                </Arrow>
+                <Arrow>
+                    <ArrowRightOutlinedIcon />
+                </Arrow>
+
 
             </Container>
         </div>
